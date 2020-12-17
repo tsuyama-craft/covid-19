27b5ec5289f9dd27d3e3d01f12ca87792671e779
TauraYouhei <yoheitaura@ezweb.ne.jp>
2020-12-17 15:58:18 +0900

    test

--- src/views/Shisetu_itiranpage.vue
+++ src/views/Shisetu_itiranpage.vue
@@ -164,19 +164,8 @@ export default {
       default:
         break;
     }
-    this.lastupdata();
   },
   methods: {
-    lastupdata: function() {
-      const req = new XMLHttpRequest();
-      req.open('GET', 'http://localhost:3000/');
-      req.onreadystatechange = function() {
-        if (req.readyState == 4 && req.status == 200) {
-          console.log(req.responseText);
-        }
-      };
-      req.send(null);
-    },
     setIryou: function(Iryou) {
       this.Iryou = Iryou
       this.tableData = this.Iryou
