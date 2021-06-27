b048c726f4908b1b12b0944f17c10db2f9904651
tsuyama-craft <82013625+kana0hi3@users.noreply.github.com>
2021-06-27 16:06:08 +0900

    Update Shisetu_itiranpage.vue

--- src/views/Shisetu_itiranpage.vue
+++ src/views/Shisetu_itiranpage.vue
@@ -122,44 +122,44 @@ export default {
   mounted: function() {
     switch(this.location_no){
       case '1':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Okayama_iryou.csv",true,'UTF8','UNICODE',this.setIryou);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Okayama_iryou.csv",true,'UTF8','UNICODE',this.setIryou);
         break;
       case '2':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Okayama_shika.csv",true,'UTF8','UNICODE',this.setShika);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Okayama_shika.csv",true,'UTF8','UNICODE',this.setShika);
         break;
       case '3':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Okayama_yakkyoku.csv",true,'UTF8','UNICODE',this.setYakkyoku);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Okayama_yakkyoku.csv",true,'UTF8','UNICODE',this.setYakkyoku);
         break;
       case '4':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Sports_location.csv",true,'SJIS','UNICODE',this.setSports);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Sports_location.csv",true,'SJIS','UNICODE',this.setSports);
         break;
       case '5':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Nursery_location.csv",true,'SJIS','UNICODE',this.setNursery);
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Kindergarten_location.csv",true,'SJIS','UNICODE',this.setKind);
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/JuniorSchool_location.csv",true,'SJIS','UNICODE',this.setJunior);
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/JuniorHighSchool_location.csv",true,'SJIS','UNICODE',this.setJuniorHigh);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Nursery_location.csv",true,'SJIS','UNICODE',this.setNursery);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Kindergarten_location.csv",true,'SJIS','UNICODE',this.setKind);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/JuniorSchool_location.csv",true,'SJIS','UNICODE',this.setJunior);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/JuniorHighSchool_location.csv",true,'SJIS','UNICODE',this.setJuniorHigh);
         break;
       case '6':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Sightseeing_location.csv",true,'SJIS','UNICODE',this.setSight);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Sightseeing_location.csv",true,'SJIS','UNICODE',this.setSight);
         break;
       case '7':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Welfare_location.csv",true,'SJIS','UNICODE',this.setWelfare);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Welfare_location.csv",true,'SJIS','UNICODE',this.setWelfare);
         break;
       case '8':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Park_location.csv",true,'SJIS','UNICODE',this.setPark);
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Library_location.csv",true,'SJIS','UNICODE',this.setLibrary);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Park_location.csv",true,'SJIS','UNICODE',this.setPark);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Library_location.csv",true,'SJIS','UNICODE',this.setLibrary);
         break;
       case '9':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Government_location.csv",true,'SJIS','UNICODE',this.setGovernment);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Government_location.csv",true,'SJIS','UNICODE',this.setGovernment);
         break;
       case '10':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Rally_location.csv",true,'SJIS','UNICODE',this.setRally);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Rally_location.csv",true,'SJIS','UNICODE',this.setRally);
         break;
       case '11':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/Children_location.csv",true,'SJIS','UNICODE',this.setChild);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/Children_location.csv",true,'SJIS','UNICODE',this.setChild);
         break;
       case '12':
-        this.GetCsvFile("https://taurayouhei.github.io/covid-19/data/AED_location.csv",true,'SJIS','UNICODE',this.setAED);
+        this.GetCsvFile("https://tsuyama-craft.github.io/covid-19/data/AED_location.csv",true,'SJIS','UNICODE',this.setAED);
         break;
       default:
         break;
